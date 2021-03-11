45d2b99dc9f2d877ddfcc0b76c3e974f05bb7ed0
OByrnes <liviabyrnes@gmail.com>
2021-03-11 18:08:09 -0500

    stage 2

--- package_tracker.py
+++ package_tracker.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 import os
 from Routes import index
+from flask_migrate import Migrate
+from app.models import db
+
 app = Flask(__name__)
 
 app.config.update({
     'SECRET_KEY': os.environ.get("SECRET_KEY"),
-    "SQLALCHEMY_TRACK_MODIFICATIONS": False
+    "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    'SQLALCHEMY_DATABASE_URI' : os.environ.get("DATABASE_URL")
 })
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 app.register_blueprint(index.bp)
