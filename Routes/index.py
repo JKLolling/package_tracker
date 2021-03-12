c175289d1d7a1fb5bbcda6a79690cd1f205691c5
Jamie Kichuk <jckichuk@gmail.com>
2021-03-11 18:11:24 -0600

    phase 2

--- Routes/index.py
+++ Routes/index.py
@@ -1,17 +1,30 @@
 from flask import Blueprint, render_template, redirect, url_for
 from app.shipping_form import ShippingForm
+from app.models import Package, db
 
 bp = Blueprint("main", __name__, url_prefix='')
 
 
 @bp.route("/")
 def main():
-    return "Package Tracker"
+    packages = Package.query.all()
+    return render_template('package_status.html', packages=packages)
 
 
 @bp.route('/new_package', methods=['GET', 'POST'])
 def new_package():
     form = ShippingForm()
     if form.validate_on_submit():
+        data = form.data
+        new_package = Package(
+            sender=data['sender'],
+            recipient=data['recipient'],
+            origin=data['origin'],
+            destination=data['destination'],
+            location=data['origin']
+        )
+        db.session.add(new_package)
+        db.session.commit()
+        Package.advance_all_locations()
         return redirect(url_for('.main'))
     return render_template('shipping_requests.html', form=form)
